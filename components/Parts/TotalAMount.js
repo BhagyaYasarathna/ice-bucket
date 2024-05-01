59374937d94c3a6513771a15bb09c8421e1969d1
Bhagya Yasarathna <bhagyayasarathna@gmail.com>
2024-05-01 18:06:48 +0530

    Summing Amount

---
+++ components/Parts/TotalAMount.js
@@ -0,0 +1,11 @@
+// TotalAmount.js
+import React from "react";
+import { View, Text } from "react-native";
+
+const TotalAmount = ({ totalAmount }) => (
+    <View>
+        <Text>Total Amount: {totalAmount}</Text>
+    </View>
+);
+
+export default TotalAmount;
