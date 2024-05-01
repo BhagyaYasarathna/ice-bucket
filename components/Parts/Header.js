5f7e6337da91edeaef9d6ed646ffc233c92adc7e
Bhagya Yasarathna <bhagyayasarathna@gmail.com>
2024-05-01 18:05:59 +0530

    Header

---
+++ components/Parts/Header.js
@@ -0,0 +1,39 @@
+import { View, Text, StyleSheet, Image } from "react-native";
+
+const Header = ({ logo }) => {
+    return (
+        <View style={styles.header}>
+            <Image source={logo} style={styles.logo} />
+            <View style={styles.titleContainer}>
+                <Text style={styles.title}>Ice Bucket</Text>
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    header: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "gray",
+        padding: 10,
+    },
+    logo: {
+        width: 40,
+        height: 40,
+        marginRight: 10,
+        borderRadius: 25,
+    },
+    titleContainer: {
+        flex: 1,
+        alignItems: "center",
+    },
+    title: {
+        fontSize: 50,
+        fontWeight: "bold",
+        color: "lightyellow",
+    },
+});
+
+export default Header;
