dd8d6ea4243d64c5f2c4eeec52c671db0481d9e5
Bhagya Yasarathna <bhagyayasarathna@gmail.com>
2024-05-01 18:08:08 +0530

    Confirmation Page

---
+++ components/OrderPlaced/OrderPlaced.js
@@ -0,0 +1,37 @@
+// OrderPlacedScreen.js
+import React from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+import Header from "../Parts/Header";
+import { Center } from "native-base";
+
+const OrderPlacedScreen = ({ navigation }) => {
+    const handleGoBack = () => {
+        // Navigate back to the home screen
+        navigation.navigate("Home");
+    };
+
+    return (
+        <View>
+            <Header title={"Ice Bucket"} />
+            <Text style={styles.details}>Your order is placed. Thank you!</Text>
+            <View style={styles.bar}>
+                <Button title="Go Back to Home" onPress={handleGoBack} />
+            </View>
+        </View>
+    );
+};
+
+export default OrderPlacedScreen;
+
+const styles = StyleSheet.create({
+    details: {
+        fontSize: 20,
+        marginTop: 20,
+        marginBottom: 100,
+        // textAlign: Center,
+    },
+    bar: {
+        marginVertical: 20,
+        alignItems: "center",
+    }
+});
